0f1952f3f8656d62193a2f62e740b3e4c510ec61
Hiroki.H <hon87kot12.xp@gmail.com>
2017-02-03 15:19:54 -0800

    Fixed some inspections, and add the function of showing up toast when received broadcast intent.

--- Quiz2/app/src/main/java/jp/honkot/exercize/basic/quiz2/MyReceiver.java
+++ Quiz2/app/src/main/java/jp/honkot/exercize/basic/quiz2/MyReceiver.java
@@ -3,6 +3,7 @@ package jp.honkot.exercize.basic.quiz2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Just send intent to move to SecondActivity
@@ -10,6 +11,8 @@ import android.content.Intent;
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, R.string.receiver_toast, Toast.LENGTH_LONG).show();
+
         Intent moveToNext = new Intent(context, SecondActivity.class);
         moveToNext.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(moveToNext);

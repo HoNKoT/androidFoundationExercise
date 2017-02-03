07c26801f20d71a18360cbbcafccce49a461dbb3
Hiroki.H <hon87kot12.xp@gmail.com>
2017-02-03 13:50:12 -0800

    Build in data binding

--- Quiz2/app/src/main/java/jp/honkot/exercize/basic/quiz2/MainActivity.java
+++ Quiz2/app/src/main/java/jp/honkot/exercize/basic/quiz2/MainActivity.java
@@ -1,13 +1,35 @@
 package jp.honkot.exercize.basic.quiz2;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import jp.honkot.exercize.basic.quiz2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        
+        binding.sendBroadcastButton.setOnClickListener(this);
+        binding.sendNotificationButton.setOnClickListener(this);
+        binding.showCreditButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sendBroadcastButton:
+                break;
+            case R.id.sendNotificationButton:
+                break;
+            case R.id.showCreditButton:
+                break;
+        }
     }
 }

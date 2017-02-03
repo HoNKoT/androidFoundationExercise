bccf27bcd3226d20e020ea455f0e1bedc768b0f7
Hiroki.H <hon87kot12.xp@gmail.com>
2017-02-03 14:12:33 -0800

    Add the function of notification

--- Quiz2/app/src/main/java/jp/honkot/exercize/basic/quiz2/MainActivity.java
+++ Quiz2/app/src/main/java/jp/honkot/exercize/basic/quiz2/MainActivity.java
@@ -1,9 +1,12 @@
 package jp.honkot.exercize.basic.quiz2;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import jp.honkot.exercize.basic.quiz2.databinding.ActivityMainBinding;
@@ -29,8 +32,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent broadcastIntent = new Intent(this, MyReceiver.class);
                 sendBroadcast(broadcastIntent);
                 break;
+
             case R.id.sendNotificationButton:
+                NotificationCompat.Builder builder =
+                        new NotificationCompat.Builder(this)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle(getString(R.string.notification_title))
+                                .setContentText(getString(R.string.notification_summary));
+                NotificationManager manager =
+                        (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0, builder.build());
                 break;
+
             case R.id.showCreditButton:
                 break;
         }
